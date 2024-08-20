14033106991b2237035ef6c997fdb7b4c4251154
Piyush1532 <piyushadav503@gmail.com>
2024-08-20 14:27:31 +0530

    added new route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
 import Create from './components/Create'
+import Update from './components/Update'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <Routes>
 <Route path='/' element={<Home/>}/>
 <Route path='/create' element={<Create/>}/>
+<Route path='/edit/:id' element={<Update/>}/>
 
     </Routes>
     </BrowserRouter>
