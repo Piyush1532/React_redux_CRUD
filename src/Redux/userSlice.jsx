3adea489cb560cdf1b050873b4984276ca62a293
Piyush1532 <piyushadav503@gmail.com>
2024-08-20 14:29:50 +0530

    added new reducers

--- src/Redux/userSlice.jsx
+++ src/Redux/userSlice.jsx
@@ -7,8 +7,19 @@ const userSlice = createSlice({
     name: "users",
     initialState:userList,
     reducers: {
-        
-    }
+        addUser:(state,action)=>{
+           state.push(action.payload)
+        },
+        updateUser:(state,action)=>{
+         const {id,name,email}=action.payload
+         const uu=state.find(user=>user.id ==id)
+         if (uu) {
+            uu.name=name,
+            uu.email=email
+         }
+        }
+        }
 });
 
+export const {addUser,updateUser} =userSlice.actions
 export default userSlice;
